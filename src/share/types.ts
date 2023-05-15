5b341d308bb56411bfbad2e27479cbbf52d5ed16
Enrique SFranco <enrique.sfranco04@gmail.com>
2023-05-15 13:55:57 -0600

    seccion de proyectos

--- src/share/types.ts
+++ src/share/types.ts
@@ -1,6 +1,7 @@
-export interface ItemDimension {
-  width: number;
-  height: number;
-  top: number;
-  left: number;
+export interface Project {
+  name: string;
+  description: string;
+  skills: string[]
+  repository: string;
+  deploy: string;
 }
